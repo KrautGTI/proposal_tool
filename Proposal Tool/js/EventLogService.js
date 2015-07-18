4b2922e8d89291e6c99d5d526065a2399b4b9daf
pankaj <pankaj16pandey@gmail.com>
2015-07-18 11:02:32 +0100

    first commit

---
+++ Proposal Tool/js/EventLogService.js
@@ -0,0 +1,158 @@
+﻿
+
+
+app.service('eventLogService', ['$http', 'sessionId', function ($http, sessionId) {
+
+    var _autoFlushIntervalSeconds = 10000; // 10 seconds
+    var _timeoutIntervalReference = null;
+    var _logs = [];
+
+
+
+    // force logs to be pushed to the server
+    var flush = function () {
+
+        if (_logs.length > 0) {
+
+            // send them up to the server
+            $http.post('api/ProposalTool/SaveADP', { DataPoints: _logs });
+
+            _logs = [];
+        }
+
+    }
+
+    // add a log to the log queue
+    var addLog = function (name, value) {
+
+        _logs.push({
+            Name: name,
+            Value: value,
+            SessionId: sessionId,
+            CreatedDate: GetTimeStamp()
+        });
+
+        // When we add a log we'll also enable the interval that auto submits our logs based on the _autoFlushIntervalSeconds
+  
+        if (_timeoutIntervalReference == null) {
+            _timeoutIntervalReference = setInterval(flush, _autoFlushIntervalSeconds);
+        }
+
+    }
+
+    // Specify how often to flush the log
+    var setFlushIntervalSeconds = function (seconds) {
+        _autoFlushIntervalSeconds = seconds * 1000;
+        clearInterval(_timeoutIntervalReference);
+    }
+
+    function GetTimeStamp() {
+        var a = new Date();
+        var year = a.getUTCFullYear();
+        var month = ("0" + (a.getUTCMonth() + 1)).slice(-2);
+        var date = ("0" + a.getUTCDate()).slice(-2);
+        var hour = ("0" + a.getUTCHours()).slice(-2);
+        var min = ("0" + a.getUTCMinutes()).slice(-2);
+        var sec = ("0" + a.getUTCSeconds()).slice(-2);
+        var milli = a.getUTCMilliseconds();
+        var time = month + '/' + date + '/' + year + ' ' + hour + ':' + min + ':' + sec + "." + milli;
+        return time;
+    }
+
+    return {
+        //createLog: createLog,
+        addLog: addLog,
+        flush: flush,
+        setFlushIntervalSeconds: setFlushIntervalSeconds
+    };
+
+}]);
+
+app.directive('eventLog', ['$filter', '$browser','eventLogService', function ($filter, $browser, eventLogService) {
+    return {
+        link: function ($scope, $element, $attrs, ngModelCtrl) {
+
+            var listener = function () {
+
+                handleElement($element, $attrs);
+            }
+
+            setEvents($element);
+
+
+
+
+
+            function handleElement($element, $attrs) {
+
+                var name = null;
+                var value = null;
+                var attrs = $attrs.eventLog.split(":");
+
+                switch ($element[0].tagName) {
+
+                    case "INPUT":
+                    case "SELECT":
+                    case "TEXTAREA":
+                        name = attrs[0];
+                        value = $element.val();
+                        break;
+                    case "DIV":
+                    case "BUTTON":
+                    case "SPAN":
+                    case "IMG":
+                    case "TD":
+                    case "TR":
+                    case "P":
+                    case "A":
+                        name = attrs[0];
+                        value = attrs[1];
+                        break;
+                    default:
+                        throw "Event Logger - <" + $element[0].tagName + "> not implemented for logging";
+                        break;
+                }
+
+
+                return eventLogService.addLog(name, value);
+
+            }
+
+            function setEvents() {
+
+
+                switch ($element[0].tagName) {
+
+                    case "INPUT":
+                    case "SELECT":
+
+                        $element.bind('change', listener)
+
+                        $element.bind('paste cut', function () {
+                            $browser.defer(listener)
+                        })
+
+                        break;
+                    case "DIV":
+                    case "BUTTON":
+                    case "SPAN":
+                    case "IMG":
+                    case "TD":
+                    case "TR":
+                    case "P":
+                    case "A":
+                        $element.bind('click', listener)
+                        break;
+                    default:
+                        throw "Event Logger - <" + $element[0].tagName + "> not implemented for logging";
+                        break;
+                }
+
+
+            }
+
+
+        }
+
+    }
+}]);
---
+++ Proposal Tool/js/controller.js
@@ -0,0 +1,140 @@
+var propTool = angular.module('propTool', ['ngRoute']);
+propTool.controller("proposalTool" , ['$scope', function($scope){
+	$scope.address1 = "115 Cordova St";
+	$scope.address2 = "Vallejo, 94591";
+	$scope.showElec = false;
+	$scope.toggleElect= function()
+	{
+		if($scope.showElec)
+			$scope.showElec = false;
+		else
+			$scope.showElec = false;
+		alert("justOneBill");
+	}
+	$scope.toggle = function() {
+       alert("hello");
+    };
+}]);
+propTool.controller('justOneBillController',['$scope', function($scope){
+        $scope.custom = true;
+        $scope.toggleCustom = function() {
+            $scope.custom = $scope.custom === false ? true: false;
+        };
+	    $scope.onDollarChange = function () {	    	
+			
+			$scope.valueKwh = $scope.dollar* 10;	
+	    	
+		}
+	    $scope.onKwhChange = function () { 		
+	    	
+			$scope.dollar = $scope.valueKwh/10;
+	    	
+		}
+		
+		
+	}]);
+
+propTool.controller('multipleBillController',['$scope', function($scope){
+        $scope.custom = true;
+        $scope.toggleCustom = function() {
+            $scope.custom = $scope.custom === false ? true: false;
+        };
+		
+	}]);
+propTool.controller('heroSummaryController',['$scope', function($scope){
+        $scope.showHide = true;
+        $scope.showHideDetail = function() {
+            $scope.showHide = $scope.showHide === false ? true: false;
+        };
+		
+	}]);
+
+propTool.config(['$routeProvider',
+				 function($routeProvider){
+					$routeProvider.
+					when('/', {
+					templateUrl: 'proposalFirstPage.html',	
+					controller:'proposalTool'	
+				    }).
+					 when('/proposalSecondPage/id1', {
+					templateUrl: 'proposalSecondPage.html',
+					controller:'proposalTool'	
+				    }).
+					when('/startProposal/id1', {
+					templateUrl: 'startProposal.html'
+				    }).
+					 when('/review/id1', {
+					templateUrl: 'review.html'
+				    }).
+					when('/multipleBills/id1', {
+					templateUrl: 'multipleBills.html',
+					controller:'multipleBillController'	
+				    }).
+					when('/justOneBill/id1', {
+					templateUrl: 'justOneBill.html',
+					controller:'justOneBillController'	
+				    }).					
+					when('/multipleBillBarGraph/id1', {
+					templateUrl: 'multipleBillBarGraph.html'
+				    }).
+					when('/oneBillBarGraph/id1', {
+					templateUrl: 'oneBillBarGraph.html'
+				    }).
+					when('/noBillBarGraph/id1', {
+					templateUrl: 'noBillBarGraph.html'
+				    }).
+					 when('/annualElectricBill/id1', {
+					templateUrl: 'annualElectricBill.html'
+				    }).
+					when('/lineGraph/id1', {
+					templateUrl: 'lineGraph.html'
+				    }).
+					 when('/percentageChange/id1', {
+					templateUrl: 'percentageChange.html'						
+				    }).	
+					 when('/futureElectricCost/id1', {
+					templateUrl: 'futureElectricCost.html'
+				    }).
+					 when('/yourOptions/id1', {
+					templateUrl: 'yourOptions.html'
+				    }).	
+					 when('/solarSystemWork/id1', {
+					templateUrl: 'solarSystemWork.html'
+				    }).
+					 when('/buildSolarSystem/id1', {
+					templateUrl: 'buildSolarSystem.html'
+				    }).
+					 when('/estimatedSolarSystemProduction/id1', {
+					templateUrl: 'estimatedSolarSystemProduction.html'
+				    }).
+					 when('/solarSystemSummary/id1', {
+					templateUrl: 'solarSystemSummary.html'
+				    }).	
+					 when('/paymentOptions/id1', {
+					templateUrl: 'paymentOptions.html'
+				    }).
+					 when('/summary/id1', {
+					templateUrl: 'summary.html'
+				    }).	
+					 when('/heroSummary/id1', {
+					templateUrl: 'heroSummary.html',
+					controller:'heroSummaryController'	
+				    }).
+					 when('/finishApplication/id1', {
+					templateUrl: 'finishApplication.html'
+				    });				 
+				 }]);	
+				
+				
+
+
+
+
+
+
+
+
+
+
+
+
